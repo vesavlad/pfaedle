79c57dd9bedc3ad2c16449dd6c35b51097c1ded2
Vlad Vesa <hello@vladvesa.ro>
2021-01-03 11:17:52 +0200

    monitor how much the app runs

--- src/apps/pfaedle/main.cpp
+++ src/apps/pfaedle/main.cpp
@@ -4,10 +4,12 @@
 
 #include "app.h"
 #include <logging/logger.h>
+#include <logging/scoped_timer.h>
 
 int main(int argc, char* argv[])
 {
     logging::configure_logging();
+    logging::scoped_timer master_timer("application");
 
     app app(argc, argv);
     app.run();
